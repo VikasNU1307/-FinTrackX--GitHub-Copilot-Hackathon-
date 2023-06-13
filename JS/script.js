55736f144101501ec754cc095508c5380fe2a43d
Vikas Umbadwar <96050488+VikasUmbadwar@users.noreply.github.com>
2023-06-13 23:24:58 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -43,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function clearForm() {
       descriptionInput.value = "";
       amountInput.value = "";
+        
       dateInput.value = "";
     }
   });
